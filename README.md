945cdec92c205be4b6513d137f397b5a3371bc55
kgowru <k.gowru@gmail.com>
2017-06-02 00:06:00 +0000

    updated to post to IoT Cloud

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # RFID Reader for IoT Lego City
 This is a node app running on a raspberry pi 3 that reads the RFID tag and then identifies the user from Salesforce
+ion
 
+### Installation
+Please follow steps 1-4 on this github [readme](https://github.com/ocsacesar/rc522). 
 ### Getting Started
 Run the following in the terminal
 ```
--- index.js
+++ index.js
@@ -5,10 +5,12 @@ var express = require('express'),
 	rc522 = require('rc522'),
 	jsforce = require('jsforce');
 
-var sfConn = new jsforce.Connection({});
-sfConn.login("kapil.gowru@iotsupporttraining.com", "test!234", function(err, userInfo){
+var sfConn = new jsforce.Connection({
+	loginUrl: 'https://corsa04-perfeng2-2015139045.vpod.t.force.com/'
+});
+
+sfConn.login("legocity@iot.com", "legolego", function(err, userInfo){
 	if (err) { return console.error(err); } 
-	
 	console.log('logged into salesforce');
 });
 
@@ -22,12 +24,22 @@ io.on('connection', function(socket){
 	console.log('a user connected');
 	rc522(function(rfidSerialNumber){
 		if(rfidSerialNumber){
-			sfConn.query("SELECT Name from Contact WHERE rfid__c =\'" + rfidSerialNumber + "\'", function(err, result){
+			sfConn.query("SELECT Name, Id from Contact WHERE rfid__c =\'" + rfidSerialNumber + "\'", function(err, result){
 				if(err) { io.emit('rfid', ''); return console.error(err);} 
 				console.log('ths is the rfid: ' + rfidSerialNumber);
 				console.log('return salesforce respone: ' + JSON.stringify(result));
-				if(result && result.records && result.records.length !== 0 && result.records[0].Name) 
+				if(result && result.records && result.records.length !== 0 && result.records[0].Name) {
 					io.emit('rfid', result.records[0].Name); 
+					sfConn.sobject('checkin__e').create({
+						location__c: 'restaurant',
+						userid__c:result.records[0].Id,
+						time__c: new Date().toISOString()
+						}, function(err, result){
+						if(err) return console.error(err);
+						console.log('checkin event sent to IoT Cloud');
+						});
+				}
+					
 				else 
 					io.emit('rfid', null);
 			});
