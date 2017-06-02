617f2edbce77b0b831996301f0d3ceb9ee77690a
Lin Hong <lhong@salesforce.com>
2017-06-02 01:26:27 -0700

    try LCD

--- index.js
+++ index.js
@@ -24,6 +24,46 @@ io.on('connection', function(socket){
 	console.log('a user connected');
 	rc522(function(rfidSerialNumber){
 		if(rfidSerialNumber){
+
+//LCD start
+var Lcd = require('lcd'),
+  lcd = new Lcd({
+    rs: 12,
+    e: 21,
+    data: [5, 6, 17, 18],
+    cols: 16,
+    rows: 1
+  });
+ 
+lcd.on('ready', function() {
+  lcd.setCursor(16, 0);
+  lcd.autoscroll();
+  print('Hello, World! ** ');
+});
+ 
+function print(str, pos) {
+  pos = pos || 0;
+ 
+  if (pos === str.length) {
+    pos = 0;
+  }
+ 
+  lcd.print(str[pos]);
+ 
+  setTimeout(function() {
+    print(str, pos + 1);
+  }, 300);
+}
+ 
+// If ctrl+c is hit, free resources and exit.
+process.on('SIGINT', function() {
+  lcd.clear();
+  lcd.close();
+  process.exit();
+});
+
+//LCD end
+
 			sfConn.query("SELECT Name, Id from Contact WHERE rfid__c =\'" + rfidSerialNumber + "\'", function(err, result){
 				if(err) { io.emit('rfid', ''); return console.error(err);} 
 				console.log('ths is the rfid: ' + rfidSerialNumber);// e558cd65  c78ba1d5
